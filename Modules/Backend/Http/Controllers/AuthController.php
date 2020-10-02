6dac800248b48ff3de89979559a59fa1207affe8
quach hoai nam <nam.quach@dragonflyteam.com>
2020-10-02 17:31:07 +0700

    initial

---
+++ Modules/Backend/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\Backend\Http\Controllers;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class AuthController extends Controller
+{
+   public function login()
+   {
+
+   }
+}
---
+++ Modules/Backend/Routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::prefix('backend')->group(function() {
+    Route::get('/auth/login', 'AuthController@login')->name('backend.auth.login');
+    Route::get('auth/logout', 'AuthController@logout')->name('backend.auth.logout');
+    Route::post('auth/login', 'AuthController@checkLogin')->name('backend.auth.checkLogin');
+    Route::post('auth/forgot-password', 'AuthController@forgotPassword')->name('backend.auth.forgotPassword');
+
+    Route::get('/', 'BackendController@index');
+});
